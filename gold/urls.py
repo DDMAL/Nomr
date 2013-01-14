18e4cb8259df863585ff09b06af5e1650d256363
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2013-01-14 17:37:02 -0500

    Home page
    Created basic homepage using default Twitter Bootstrap css (we can
    modify the look and feel later). Also included jquery library.

--- gold/urls.py
+++ gold/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'gold.views.home', name='home'),
+    url(r'^$', 'gold.views.home', name='home'),
     # url(r'^gold/', include('gold.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- gold/views.py
+++ gold/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def home(request):
+    '''
+    Serve the home page
+    '''
+
+    return render_to_response('index.html', context_instance=RequestContext(request))
