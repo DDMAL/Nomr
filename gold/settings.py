52adfa5748465be0e6f6e6b99e3bd6c98bf211e6
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2013-01-15 16:39:35 -0500

    Add django REST framework
    to installed apps.

--- gold/settings.py
+++ gold/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gold',
+    'rest_framework',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
