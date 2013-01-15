b3fdf6f1e96ef653c8ea705676bac822896f7fee
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2013-01-15 16:10:31 -0500

    Serve media root
    Now one can click a link to an image/mei file and view it in the web
    browser.

--- gold/urls.py
+++ gold/urls.py
@@ -1,16 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'gold.views.home', name='home'),
-    # url(r'^gold/', include('gold.foo.urls')),
+    url(r'^pages/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^admin/', include(admin.site.urls))
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
