1ffd0f8229245bdb4242ba43c2b67a3f17c21662
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2013-01-15 17:37:44 -0500

    Model serializers
    Created serialization classes for Books, BookParts, and Pages.

---
+++ gold/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from gold.models import Book, BookPart, Page
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+            'uuid',
+            'title',
+            'description',
+            'is_manuscript',
+            'printers',
+            'publication_date',
+            'location',
+            'printing_technology',
+            'genre',
+            'is_sacred',
+            'alternate_id'
+        )
+
+class BookPartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookPart
+        fields = ('uuid', 'book', 'part_type')
+
+class PageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Page
+        fields = (
+            'uuid', 
+            'book_part',
+            'image',
+            'image_width',
+            'image_height',
+            'mei',
+            'sequence'
+        )
