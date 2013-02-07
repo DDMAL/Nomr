5207bd08735c45264988c554b24974ffed8b7cc3
gburlet <gregory.burlet@mail.mcgill.ca>
2013-02-06 19:43:20 -0500

    Solr search
    Begin setting up solr search. Added Book model fields to the solr
    indexing schema.

--- nomr/settings.py
+++ nomr/settings.py
@@ -23,6 +23,8 @@ DATABASES = {
     }
 }
 
+SOLR_SERVER = 'http://localhost:8983/solr'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
